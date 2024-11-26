05c4a0882f2c4aac258f5df69d9b3d01ec83e7d4
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-26 03:49:54 -0500

    Font Awesome icons, date and style header & main section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
+import Data from './Data.jsx';
 
-import Data from './Data.jsx'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInbox } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">
-        <div>Archive all calls</div>
+        <div className='archive-calls'>
+          <div className='call-type'>
+            <FontAwesomeIcon icon={faInbox}/><span>Archive all calls</span>
+          </div>
+        </div>
         <div>
           <Data/>
         </div>
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,4 +1,6 @@
 import useSWR from 'swr';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneVolume, faPhoneSlash } from '@fortawesome/free-solid-svg-icons';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -13,22 +15,29 @@ const Data = () => {
   if (isValidating) return <div className="Loading">Loading...</div>;
 
   return (
-    <div>
+    <div className='inbox'>
       {calls &&
         calls.map((call, index) => (
-          <div className='call-summary' key={index}>
-            <div className='call-type'>
-                { call.call_type === 'answered' ? '📞' : '❌' }
-            </div>
-
-            <div>
-                <p className='call-to'>{ call.to }</p>
-                <p>tried to call on { call.from }</p>
-            </div>
-
-            <p className='call-time'>
-              {new Date(call.created_at).toLocaleTimeString('en-US',{timeZone:'UTC',hour12:true,hour:'numeric',minute:'numeric'})}
+          <div>
+            <p className='call-date'>
+              {new Date(call.created_at).toLocaleDateString('en-US', {month:'long',day:'numeric',year:'numeric'}).replace(/,/g, '').replace(/(\s)/, ", ")}
             </p>
+
+            <div className='call-summary' key={index}>
+              <div className='call-type'>
+                  { call.call_type === 'answered' ? <FontAwesomeIcon icon={faPhoneVolume} style={{color: "#34b31d"}}/> : <FontAwesomeIcon icon={faPhoneSlash} style={{color: "#fc5624"}}/> }
+              </div>
+
+              <div>
+                  <p>{ call.from }</p>
+                  <p className='call-to'>tried to call on { call.to }</p>
+              </div>
+
+              <p className='call-time'>
+                {new Date(call.created_at).toLocaleTimeString('en-US',{timeZone:'EST',hour12:true,hour:'numeric',minute:'numeric'})}
+                <span></span>
+              </p>
+            </div>
           </div>
         ))}
     </div>
